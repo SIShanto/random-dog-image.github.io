b42df9ce6d1c394ebf055808f16175c419c6f5d3
Shafiul Islam <96788338+SIShanto@users.noreply.github.com>
2022-09-02 12:45:28 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
  const dogImageDiv=document.querySelector('#dogimage');
+ const dogButton=document.querySelector('#dogButton');
 
-fetch('https://dog.ceo/api/breeds/image/random').then( response=>response.json())
-.then(json=>{
-    dogImageDiv.innerHTML=`<img src='${json.message}'/>`
+function dog()
+{
+    fetch('https://dog.ceo/api/breeds/image/random')
+    .then( response=>response.json())
+    .then(json=>{
+    dogImageDiv.innerHTML=`<img src='${json.message}'height=500 width=600/>`
+})
+}
+
+dogButton.addEventListener("click",()=>
+{
+   dog();
 })
